8ee1a43cc20778d5c51baa68479812e7849f7205
dekatotoro <nekoganeru@gmail.com>
2016-11-08 11:15:13 +0900

    Remove unnecessary shift operation

--- ColorAdjuster/ColorAdjuster.swift
+++ ColorAdjuster/ColorAdjuster.swift
@@ -116,7 +116,7 @@ extension UIColor {
     public convenience init(hex: Int, alpha: CGFloat = 1) {
         let red = CGFloat((hex & 0xFF0000) >> 16) / 255
         let green = CGFloat((hex & 0x00FF00) >> 8 ) / 255
-        let blue = CGFloat((hex & 0x0000FF) >> 0 ) / 255
+        let blue = CGFloat(hex & 0x0000FF) / 255
         
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
